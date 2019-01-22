2359717deb94007c4532812a154548c72253f939
Chromeo <cristiansabino@yahoo.com>
2019-01-21 16:30:22 -0800

    Starting code for button pressing

---
+++ Assets/Rocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ProcessInput();    
+    }
+
+    private void ProcessInput() {
+
+        if (Input.GetKey(KeyCode.Space)) {
+            Debug.Log("up and away");
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("going left");
+        }else if (Input.GetKey(KeyCode.D)) {
+            Debug.Log("going right");
+        }
+    }
+}
