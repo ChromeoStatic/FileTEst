8a6bef1bed87d7e84e095f3ccad5212463a4ff8e
Chromeo <cristiansabino@yahoo.com>
2019-01-21 18:47:39 -0800

    Created vector, changed sounds

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,8 +7,8 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rb;
     AudioSource rocket;
-    public float speed = 3.0f;
-
+    public Vector3 speed = new Vector3(0,1000.0f,0);
+    public Vector3 velocityRot = new Vector3(0, 0, 80.0f);
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,23 +18,31 @@ public class Rocket : MonoBehaviour
 
     void Update()
     {
-        ProcessInput();    
+      
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput() {
-
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) {
-            rb.AddRelativeForce(Vector3.up);
+            rb.AddRelativeForce(speed*Time.deltaTime);
             if (rocket.isPlaying == false) {
                 rocket.Play();
             }
         } else {
             rocket.Stop();
         }
+    }
+    private void Rotate() {
+
+        rb.freezeRotation = true;   //take manual control of rotation when used
+
         if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(0,0,speed*Time.deltaTime);
+            transform.Rotate(velocityRot * Time.deltaTime);
         }else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(0,0,-speed*Time.deltaTime);
+            transform.Rotate(-velocityRot * Time.deltaTime);
         }
+
+        rb.freezeRotation = false;  //resume physics rotation 
     }
 }
